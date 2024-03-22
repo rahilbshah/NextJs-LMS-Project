bc34bf136eab23cc88974e503955329dddd68ba5
rahilbshah <98266707+rahilbshah@users.noreply.github.com>
2024-03-22 18:09:42 +0530

    Add Final Dashboard and Ananlytics Page

--- app/(dashboard)/(routes)/teacher/layout.tsx
+++ app/(dashboard)/(routes)/teacher/layout.tsx
@@ -1,10 +1,4 @@
 const TeacherLayout = ({ children }: { children: React.ReactNode }) => {
-  // const { userId } = auth();
-
-  // if (!isTeacher(userId)) {
-  //   return redirect("/");
-  // }
-
   return <>{children}</>;
 };
 
--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -5,8 +5,6 @@ import { UploadThingError } from "uploadthing/server";
 const f = createUploadthing();
 const handleAuth = () => {
   const { userId } = auth();
-  //   const isAuthorized = isTeacher(userId);
-
   if (!userId) throw new Error("Unauthorized");
   return { userId };
 };
